d58118a177a24333c1d4c8e364a62d2d3039a5d9
Yago Nitto <yago_nitto@hotmail.com>
2025-12-10 18:42:38 -0300

    feat: add cryptography to password and verify email already exists

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
 import { prisma } from '@/lib/prisma'
@@ -11,11 +12,21 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const passwordHash = await hash(password, 8)
+
+  const userAlreadyExists = await prisma.user.findUnique({
+    where: { email },
+  })
+
+  if (userAlreadyExists) {
+    return reply.status(409).send()
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      passwordHash: password,
+      passwordHash,
     },
   })
 
