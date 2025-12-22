7a6acb3f2bd8a8edf61827592dbdbb1a29c7878d
Yago Nitto <yago.nitto@outlook.com>
2025-12-22 12:14:21 -0300

    test: add tests to validate check in date

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,10 +1,33 @@
 import { randomUUID } from 'node:crypto'
+import dayjs from 'dayjs'
 import type { CheckIn } from 'generated/prisma/client'
 import type { CheckInUncheckedCreateInput } from 'generated/prisma/models'
 import type { CheckInsRepository } from '../check-ins-repository'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
+
+  async findByUserIdOnDate(
+    userId: string,
+    date: Date,
+  ): Promise<CheckIn | null> {
+    const startOfDay = dayjs(date).startOf('date')
+    const endOfDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.createdAt)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfDay) && checkInDate.isBefore(endOfDay)
+
+      return checkIn.userId === userId && isOnSameDate
+    })
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+
+    return checkInOnSameDate
+  }
 
   async create(data: CheckInUncheckedCreateInput): Promise<CheckIn> {
     const checkIn = {
